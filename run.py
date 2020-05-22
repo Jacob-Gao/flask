b98b49309466c759ea3df69362d1bbaa03c2bfd7
高晨鹏 <gaochenpeng@xiaoxiangyoupin.cn>
2020-05-22 10:22:18 +0800

    gao

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from pro_flask import create_app
 
 
-if __name__ == '__main__':
-    app = create_app('settings.Config')
-    app.run(host='0.0.0.0')
+app = create_app('settings.Config')
+app.run(host='0.0.0.0')
 
