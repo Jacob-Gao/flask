10c32887e185e869210792a9b80371bc2bce81f1
高晨鹏 <gaochenpeng@xiaoxiangyoupin.cn>
2020-06-17 09:04:58 +0800

    make code stronger

--- pro_flask/app01/views/initial_ecs.py
+++ pro_flask/app01/views/initial_ecs.py
@@ -37,13 +37,8 @@ def work_loop(y_host, y_user, y_passwd, host, t_user, t_passwd):
         command=
         '/bin/bash /root/copy_jdk_shell/copy_initial_shell.sh {} > /root/copy_jdk_shell/copy_initial_shell.log 2>&1'
             .format(host))  # yunwei machine log
-
-    excute_command(
-        client=client,
-        command=
-        '/usr/bin/expect /root/copy_jdk_shell/id_rsa.sh {} > /root/copy_jdk_shell/copy_initial_shell.log 2>&1'
-            .format(host))
     client.close()
+
     # excute initial.sh
     client = connect_to_remote_host(hostip=host,
                                     username=t_user,
@@ -70,6 +65,27 @@ def work_loop(y_host, y_user, y_passwd, host, t_user, t_passwd):
     excute_command(client, '/bin/bash /root/next.sh >> /root/initial.log 2>&1')
     client.close()
 
+def transfer_sshkey(y_host,y_user,y_passwd,hostlist,check_chaoxie):
+    client = connect_to_remote_host(hostip=y_host,
+                                    username=y_user,
+                                    password=y_passwd)
+    for i in range(len(hostlist)):
+        # 传密钥
+        if check_chaoxie:
+            excute_command(
+                client=client,
+                command=
+                '/usr/bin/expect /root/copy_jdk_shell/id_rsa_chaoxie.sh {} >> /root/copy_jdk_shell/copy_initial_shell.log 2>&1'
+                    .format(hostlist[i]))
+        else:
+            excute_command(
+                client=client,
+                command=
+                '/usr/bin/expect /root/copy_jdk_shell/id_rsa_weibofenqi.sh {} >> /root/copy_jdk_shell/copy_initial_shell.log 2>&1'
+                    .format(hostlist[i]))
+
+    client.close()
+
 
 def yunwei_log_catch(y_host, y_user, y_passwd):
     client = connect_to_remote_host(hostip=y_host,
@@ -94,7 +110,6 @@ def target_machine_log_catch(host, t_user, t_passwd, log):
     return target_machine_log
 
 
-
 @app01.route('/index.html', methods=['GET', 'POST'])
 def login():
     return render_template('index.html')
@@ -113,62 +128,64 @@ def initial():
         return render_template('initial-ecs.html')
     else:
         ip_address = request.form.get('ip_address')
+        check_chaoxie = request.form.get('check-chaoxie')
         hostlist = ip_address.split()
         faild_list = list()
         t_user = "root"
-        t_passwd = "jacob"
+        t_passwd = "xY6#1WgBj2kR8l4fg"
         y_host = "121.40.94.165"
         y_user = "root"
-        y_passwd = "jacob"
+        y_passwd = "xY6#1WgBj2kR8l4fg"
 
-        for i in hostlist:
+        for i1 in hostlist:
             try:
                 client = paramiko.client.SSHClient(
                 )  # A high-level representation of a session with an SSH server
                 client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
                 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
-                client.connect(i, username='root', password='jacob', timeout=4)
+                client.connect(i1, username='root', password='xY6#1WgBj2kR8l4fg', timeout=4)
             except Exception as e:
                 print(e)
-                faild_list.append(i)
-            finally:
+                faild_list.append(i1)
+            else:
                 client.close()
 
-        for i in faild_list:
-            hostlist.remove(i)
+        for i2 in faild_list:
+            hostlist.remove(i2)
 
-        for i in hostlist:
+        for i4 in hostlist:
             client = paramiko.client.SSHClient()  # A high-level representation of a session with an SSH server
             client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
-            client.connect(i, username='root', password='jacob', timeout=4)
+            client.connect(i4, username='root', password='xY6#1WgBj2kR8l4fg', timeout=4)
             sftp = client.open_sftp()
             try:
                 sftp.stat('/data/tinyplat/')
                 print("*" * 100)
-                print("机器已经初始化过了，很有可能正在生产环境使用，请检查ip是否输入正确,ip地址为{}".format(i))
+                print("机器已经初始化过了，很有可能正在生产环境使用，请检查ip是否输入正确,ip地址为{}".format(i4))
                 print("*" * 100)
                 client.close()
-                hostlist.remove(i)
+                hostlist.remove(i4)
             except Exception as e:
                 print(e)
+                client.close()
                 pass
 
         host_num = len(hostlist)
         print(hostlist)
         if host_num != 0:
-            for i in range(host_num):
-                t = threading.Thread(name='process on {}'.format(hostlist[i]),
+            for i3 in range(host_num):
+                t = threading.Thread(name='process on {}'.format(hostlist[i3]),
                                      target=work_loop,
-                                     args=(y_host, y_user, y_passwd, hostlist[i],
+                                     args=(y_host, y_user, y_passwd, hostlist[i3],
                                            t_user, t_passwd))
                 t.start()
             time.sleep(300)
+            transfer_sshkey(y_host,y_user,y_passwd,hostlist,check_chaoxie)
             yunwei_log = yunwei_log_catch(y_host, y_user, y_passwd)
             target_machine_log = ""
-            for i in range(host_num):
+            for i in range(len(hostlist)):
                 target_machine_log = target_machine_log_catch(hostlist[i], t_user, t_passwd, target_machine_log)
             return render_template('initial-ecs.html', yunwei_log=yunwei_log, target_machine_log=target_machine_log)
         else:
             return render_template("initial-ecs.html")
-
--- test.py
+++ test.py
@@ -1,75 +1,98 @@
 import paramiko
-import sys,threading,time
+import sys, threading, time
 
-hostlist=['192.168.1.252', '192.168.1.1','192.168.1.254','192.168.1.253']
-faild_list1 = list()
-faild_list2 = list()
-def connect_to_remote_host(hostip, username='root', password='jacob'):
+
+#
+# hostlist=['192.168.1.252', '192.168.1.1','192.168.1.254','192.168.1.253']
+# faild_list1 = list()
+# faild_list2 = list()
+# def connect_to_remote_host(hostip, username='root', password='jacob'):
+#     client = paramiko.client.SSHClient(
+#     )  # A high-level representation of a session with an SSH server
+#     client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
+#     client.connect(hostname=hostip, username=username, password=password, timeout=3)
+#     return client
+#     # try:
+#     #     client.connect(hostname=hostip, username=username, password=password, timeout=3)
+#     #     return client
+#     # except Exception as e:
+#     #     print("连接服务器失败，错误信息：{}".format(e))
+#     #     exit(1)
+#
+# #
+# # def excute_command(client, command):
+# #     try:
+# #         stdin, stdout, stderr = client.exec_command(command)
+# #     except Exception as e:
+# #         client.close()
+# #         return e
+# #     else:
+# #         standout = stdout.read().decode('utf-8')
+# #         return standout
+#
+#
+#
+#
+# def check():
+#     for i in hostlist:
+#         try:
+#             client = paramiko.client.SSHClient(
+#             )  # A high-level representation of a session with an SSH server
+#             client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
+#             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
+#             client.connect(i, username='root', password='jacob',timeout=4)
+#         except Exception as e:
+#             print(e)
+#             faild_list1.append(i)
+#         finally:
+#                 client.close()
+#
+#     for i in faild_list1:
+#         hostlist.remove(i)
+#
+#     for i in hostlist:
+#         client = paramiko.client.SSHClient()  # A high-level representation of a session with an SSH server
+#         client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
+#         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
+#         client.connect(i, username='root', password='jacob',timeout=4)
+#         sftp = client.open_sftp()
+#         try:
+#             sftp.stat('/data/tinyplat/')
+#             print("*" * 100)
+#             print("机器已经初始化过了，很有可能正在生产环境使用，请检查ip是否输入正确,ip地址为{}")
+#             print("*" * 100)
+#             client.close()
+#             hostlist.remove(i)
+#         except Exception as e:
+#             print(e)
+#             pass
+#
+#
+#
+#
+#
+# check()
+# print("**********************************")
+# print(hostlist)
+def connect_to_remote_host(hostip, username, password):
     client = paramiko.client.SSHClient(
     )  # A high-level representation of a session with an SSH server
     client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
-    client.connect(hostname=hostip, username=username, password=password, timeout=3)
+    client.connect(hostname=hostip, username=username, password=password, timeout=5)
     return client
-    # try:
-    #     client.connect(hostname=hostip, username=username, password=password, timeout=3)
-    #     return client
-    # except Exception as e:
-    #     print("连接服务器失败，错误信息：{}".format(e))
-    #     exit(1)
-
-#
-# def excute_command(client, command):
-#     try:
-#         stdin, stdout, stderr = client.exec_command(command)
-#     except Exception as e:
-#         client.close()
-#         return e
-#     else:
-#         standout = stdout.read().decode('utf-8')
-#         return standout
 
 
+def excute_command(client, command):
+    try:
+        stdin, stdout, stderr = client.exec_command(command)
+
+    except Exception as e:
+        client.close()
+        print(e)
+    else:
+        standout = stdout.read().decode()
+        return standout
 
 
-def check():
-    for i in hostlist:
-        try:
-            client = paramiko.client.SSHClient(
-            )  # A high-level representation of a session with an SSH server
-            client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
-            client.connect(i, username='root', password='jacob',timeout=4)
-        except Exception as e:
-            print(e)
-            faild_list1.append(i)
-        finally:
-                client.close()
-
-    for i in faild_list1:
-        hostlist.remove(i)
-
-    for i in hostlist:
-        client = paramiko.client.SSHClient()  # A high-level representation of a session with an SSH server
-        client.load_system_host_keys()  # 读known hosts文件里的public key，没有再说
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 或者接受用WarningPolicy()
-        client.connect(i, username='root', password='jacob',timeout=4)
-        sftp = client.open_sftp()
-        try:
-            sftp.stat('/data/tinyplat/')
-            print("*" * 100)
-            print("机器已经初始化过了，很有可能正在生产环境使用，请检查ip是否输入正确,ip地址为{}")
-            print("*" * 100)
-            client.close()
-            hostlist.remove(i)
-        except Exception as e:
-            print(e)
-            pass
-
-
-
-
-
-check()
-print("**********************************")
-print(hostlist)
